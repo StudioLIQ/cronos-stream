13fd15ed6a513caa3b3a47e776f4275474a9b089
studioLIQ <contact@studioliq.com>
2026-01-22 05:05:06 +0900

    TKT-0027: Add logo and favicon

--- apps/web/src/components/TopNav.tsx
+++ apps/web/src/components/TopNav.tsx
@@ -6,6 +6,13 @@ export function TopNav({ children }: { children?: ReactNode }) {
     <header className="topbar">
       <div className="topbar-inner">
         <Link to="/" className="brand">
+          <img
+            src="/logo.svg"
+            width={20}
+            height={20}
+            alt=""
+            style={{ display: 'block' }}
+          />
           Stream402
         </Link>
         {children}
@@ -13,4 +20,3 @@ export function TopNav({ children }: { children?: ReactNode }) {
     </header>
   );
 }
-
